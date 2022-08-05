2f53deb32a548174cfdc994749b8d4bfd37f2ae0
Shuya Kinjo <a8109058@gmail.com>
2022-08-05 09:41:17 +0900

    Finish up the di container sample.

--- di/src/computer/computer.controller.ts
+++ di/src/computer/computer.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CpuService } from 'src/cpu/cpu.service';
+import { DiscService } from 'src/disc/disc.service';
 
 @Controller('computer')
-export class ComputerController {}
+export class ComputerController {
+  constructor(
+    private readonly discService: DiscService,
+    private readonly cpuService: CpuService,
+  ) {}
+  @Get()
+  run() {
+    return [this.cpuService.compute(1000000000, 2), this.discService.getData()];
+  }
+}
--- di/src/disc/disc.service.ts
+++ di/src/disc/disc.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
-
+import { PowerService } from '../power/power.service';
 @Injectable()
-export class DiscService {}
+export class DiscService {
+  constructor(private powerService: PowerService) {}
+  getData() {
+    console.log('Drawing 20 watts of power from Power Service.');
+    this.powerService.supplyPower(20);
+
+    return 'Awesome data!';
+  }
+}
